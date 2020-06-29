f4331a8c4eecddb3d9944227b7cb26908e664211
variousnabil <varionabilr@gmail.com>
2020-06-30 06:00:48 +0700

    Updates

---
+++ precache-manifest.4b5fb3e0cfa5fdffc3b7e83693e345a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c930766e9732548419086f6491989d",
+    "url": "/ToDoApp-ReactJS/index.html"
+  },
+  {
+    "revision": "1790371d97cf6c215c58",
+    "url": "/ToDoApp-ReactJS/static/css/main.935de3d8.chunk.css"
+  },
+  {
+    "revision": "cabf7eb3c10d035e1a38",
+    "url": "/ToDoApp-ReactJS/static/js/2.36f4692c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoApp-ReactJS/static/js/2.36f4692c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1790371d97cf6c215c58",
+    "url": "/ToDoApp-ReactJS/static/js/main.6374c016.chunk.js"
+  },
+  {
+    "revision": "b999ec998c92be677d06",
+    "url": "/ToDoApp-ReactJS/static/js/runtime-main.eae1d0b4.js"
+  }
+]);
